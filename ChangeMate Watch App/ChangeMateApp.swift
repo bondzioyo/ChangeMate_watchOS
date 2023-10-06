f1a6d28d882911b5f549c4698234d2bc49e579db
bondzioyo <bondzioyo@>
2023-10-06 19:43:53 +0200

    Initial Commit

---
+++ ChangeMate Watch App/ChangeMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChangeMateApp.swift
+//  ChangeMate Watch App
+//
+//  Created by Wojciech Świątek on 06/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct ChangeMate_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChangeMate Watch AppUITests/ChangeMate_Watch_AppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ChangeMate_Watch_AppUITestsLaunchTests.swift
+//  ChangeMate Watch AppUITests
+//
+//  Created by Wojciech Świątek on 06/10/2023.
+//
+
+import XCTest
+
+final class ChangeMate_Watch_AppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
